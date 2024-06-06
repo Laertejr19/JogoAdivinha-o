7bb4045e02ad551eb41178401416c10c2f56a53e
Nicolylemes <n320992@gmail.com>
2024-06-06 17:52:56 -0300

    Atualização

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function Usuario(){
+var Nome = document.getElementById("usuario").value
+if(Nome == ""){
+alert("Insira um nome para jogar");
+} else {
+    window.open("http://127.0.0.1:5500/pagina%202.html");
+}
+}
