9d4891c4e40eb81791534706795c232999de7ed0
Nicolylemes <n320992@gmail.com>
2024-06-11 17:55:51 -0300

    Atualização

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 function Usuario(){
-const Nome = toString(document.getElementById("usuario").value);
-
-if(Nome == ""){
+var valorNome = document.querySelector("#usuario").value;
+if(valorNome == ""){
 alert("Insira um nome para jogar");
 } else {
     window.open("http://127.0.0.1:5500/pagina%202.html");
 }
-
 }
