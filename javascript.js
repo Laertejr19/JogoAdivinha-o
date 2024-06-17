7eb326f688e8cc1025d81aa1c8b8e0b1df517125
Laertejr19 <laerteferrazdasilvajunior19@gmail.com>
2024-06-17 17:10:28 -0300

    Atualização

--- javascript.js
+++ javascript.js
@@ -3,7 +3,12 @@ var valorNome = document.getElementById("usuario").value;
 if(valorNome == ""){
 alert("Insira um nome para jogar");
 } else {
-   window.open("http://127.0.0.1:5500/pagina%202.html"); //pesquisar sobre
+   var Segundapg = window.open('http://127.0.0.1:5500/pagina%202.html', '_self');
+   Segundapg.document.write('<h1>Boas vindas ' + valorNome + '!</h1>');
+   Segundapg.document.write('<input type="text">');
+   Segundapg.document.write('<button>Adivinhar</button>');
+   Segundapg.document.close();
+   
 }
 
 
